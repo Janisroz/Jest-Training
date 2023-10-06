5516a45f0d8c800e1390c2b60e7f7cf925af844a
Janisroz <janisrozkalejs@gmail.com>
2023-10-06 15:20:00 +0000

    Learn how to use basic jest

---
+++ scripts/tests/calc.test.js
@@ -0,0 +1,27 @@
+/**
+ * @jest-environment jsdom
+ */
+
+const addition = require("../calc");
+
+describe("Calculator", () => {
+    describe("Addition function", ()=> {
+        test("should return 42 for 20 + 22", () =>{
+            expect(addition(20,22)).toBe(42);
+        })
+        test("should return 73 for 31 + 42", () =>{
+            expect(addition(31,42)).toBe(73);
+        })
+    });
+    describe("Subtraction function", ()=> {
+        
+    });
+    describe("Multiplication function", ()=> {
+        
+    });
+    describe("Division function", ()=> {
+        
+    });
+} )
+
+
